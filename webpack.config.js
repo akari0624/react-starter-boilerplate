9f73bdea13e1c8a0b2c2dae862d79f284d58ede8
akari0624 <akari0624@hotmail.com>
2018-06-11 13:46:02 +0800

    remove unuse dependencies, do more useful webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: ['./src/index.js'],
     output: {
@@ -14,12 +17,41 @@ module.exports = {
                 exclude: /node_modules/
             },
             {
+                use: [
+                    {
+                        loader: 'style-loader'
+                    }, {
+                        loader: 'css-loader'
+                    }, {
+                        loader: 'less-loader',
+                        options: {
+                            javascriptEnabled: true
+                        }
+                    }
+                ],
+                test: /\.less$/
+
+            },
+            {
                 use: ['style-loader', 'css-loader'],
                 test: /\.css$/
-            }
+            },
+            {
+                use:'file-loader',
+                test:/\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/
+                 
+               },
+               { use: 'url-loader?limit=100000', test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/ }
+
         ]
     },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template:path.resolve(__dirname,'index.html'),
+            filename: './index.html'
+        })
 
+    ],
     resolve: {
         extensions: ['*', '.js', '.jsx']
     },
