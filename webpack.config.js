abe1bd0a7d3a4ad15194eef0e7de563575710b8e
akari0624 <akari0624@hotmail.com>
2018-06-15 02:27:20 +0800

    add babel-polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['./src/index.js'],
+    entry: ['babel-polyfill' ,'./src/index.js'],
     output: {
         path: __dirname,
         publicPath: '/',
@@ -15,8 +15,7 @@ module.exports = {
                 use: 'babel-loader',
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/
-            },
-            {
+            }, {
                 use: [
                     {
                         loader: 'style-loader'
@@ -31,25 +30,26 @@ module.exports = {
                 ],
                 test: /\.(css|less)$/
 
-            },
-            {
-                use:'file-loader',
-                test:/\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/
-                 
-               },
-               { use: 'url-loader?limit=100000', test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/ }
+            }, {
+                use: 'file-loader',
+                test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/
+
+            }, {
+                use: 'url-loader?limit=100000',
+                test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/
+            }
 
         ]
     },
-    plugins:[
-        new HtmlWebPackPlugin({
-            template:path.resolve(__dirname,'index.html'),
+    plugins: [new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, 'index.html'),
             filename: './index.html'
-        })
-
-    ],
+        })],
     resolve: {
-        modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            'node_modules'
+        ],
         extensions: ['*', '.js', '.jsx']
     },
     devServer: {
