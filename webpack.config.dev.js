0a7612a58e25accac4e6462dad8f9ca2c775543d
akari0624 <akari0624@hotmail.com>
2018-07-23 02:39:40 +0800

    make webpack config file use 2 indent format

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,70 +3,71 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: ['babel-polyfill' ,'./src/index.js'],
-    output: {
-        path: __dirname,
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
+  entry: [
+    'babel-polyfill', './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
 
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/
-            }, {
-                use: [
-                    {
-                        loader: 'style-loader'
-                    }, {
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'less-loader',
-                        options: {
-                            javascriptEnabled: true
-                        }
-                    }
-                ],
-                test: /\.(css|less)$/
-
-            }, 
-            {
-                use: 'url-loader?limit=8192',
-                test: /\.(svg)$/
-            },
-            {
-                test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/,
-                use: [
-                    {
-                loader:'file-loader',
-                options:{
-                    name:'[path][name].[ext]',
-                    publicPath:'/'  
-                  }
-                    }]
-
-            },
-
-        ]
-    },
-    plugins: [new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
-            filename: './index.html'
-        }),        
-            new webpack.HotModuleReplacementPlugin()
-    ],
-    resolve: {
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            'node_modules'
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/
+      }, {
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true
+            }
+          }
         ],
-        extensions: ['*', '.js', '.jsx']
-    },
-    devServer: {
-        historyApiFallback: true,
-        contentBase: './',
-        port: 9999
-    }
+        test: /\.(css|less)$/
+
+      }, {
+        use: 'url-loader?limit=8192',
+        test: /\.(svg)$/
+      }, {
+        test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              publicPath: '/'
+            }
+          }
+        ]
+
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      filename: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ],
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 9999
+  }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,70 +2,69 @@ const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill' ,'./src/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',        // your webApp name(if has) on server put here.   eg:/MyWebApp
-        filename: '[name].[chunkhash].js',
-        chunkFilename:'[name].[chunkhash].js'
-    },
+  entry: [
+    'babel-polyfill', './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', // your webApp name(if has) on server put here.   eg:/MyWebApp
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
 
-    module: {
-        rules: [
-            {
-                use: 'babel-loader',
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/
-            }, {
-                use: [
-                    {
-                        loader: 'style-loader'
-                    }, {
-                        loader: 'css-loader'
-                    }, {
-                        loader: 'less-loader',
-                        options: {
-                            javascriptEnabled: true
-                        }
-                    }
-                ],
-                test: /\.(css|less)$/
-
-            },
-            {
-                use: 'url-loader?limit=8192',
-                test: /\.(svg)$/
+  module: {
+    rules: [
+      {
+        use: 'babel-loader',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/
+      }, {
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader'
+          }, {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true
             }
-            ,{
-                test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/,
-                use: [
-                    {
-                loader:'file-loader',
-                options:{
-                    name:'[path][name].[ext]',
-                    publicPath:'/'  // your webApp name(if has) on server put here.   eg:/MyWebApp
-                  }
-                    }]
-
-            },
-
-        ]
-    },
-    plugins: [new HtmlWebPackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
-            filename: './index.html'
-        })],
-    resolve: {
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            'node_modules'
+          }
         ],
-        extensions: ['*', '.js', '.jsx']
-    },   
-     optimization: {
-        splitChunks: {
-            chunks:'all'
-        }
-    },
-    
+        test: /\.(css|less)$/
+
+      }, {
+        use: 'url-loader?limit=8192',
+        test: /\.(svg)$/
+      }, {
+        test: /\.(png|jpg|gif|mp4|ogg|svg|css|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              publicPath: '/' // your webApp name(if has) on server put here.   eg:/MyWebApp
+            }
+          }
+        ]
+
+      }
+    ]
+  },
+  plugins: [new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+      filename: './index.html'
+    })],
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      'node_modules'
+    ],
+    extensions: ['*', '.js', '.jsx']
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
 };
