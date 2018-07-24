d8dd3e61ef1a0938464c9c502ab4800af4501714
akari0624 <akari0624@hotmail.com>
2018-07-24 11:22:10 +0800

    add Image to test context

--- test/test_helper.js
+++ test/test_helper.js
@@ -12,6 +12,8 @@ import reducers from '../src/reducers'
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>')
 global.window = global.document.defaultView
 global.navigator = global.window.navigator
+global.Image = global.window.Image;
+
 const $ = _$(window)
 
 chaiJquery(chai, chai.util, $)
