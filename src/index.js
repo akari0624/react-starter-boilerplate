1b67b27abdf0a0edda82d9631ce2ecb2e8478b05
akari0624 <akari0624@hotmail.com>
2018-11-02 15:33:23 +0800

    when in production mode, remove chrome REDUX devtool extension init

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ import {createStore, applyMiddleware} from 'redux'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import reducers from './reducers'
 
-import indexRouter from './indexRouter'
+import indexRouter from './mainpage'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
 
-const appStore = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+let appStore;
+
+if(process.env.NODE_ENV === 'production'){
+  
+  appStore = createStoreWithMiddleware(reducers)
+  
+}else{
+  appStore = createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+}
+
 
 ReactDOM.render(
   <Provider store={appStore}>
