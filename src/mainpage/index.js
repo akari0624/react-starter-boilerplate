e49789d5ae286df87e59828ffd342bf3871c843b
akari0624 <akari0624@hotmail.com>
2018-11-02 15:36:18 +0800

    change landing page text

--- src/mainpage/index.js
+++ src/mainpage/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Styled from 'styled-components'
+
+
+const MiddleDiv = Styled.div`
+  text-align:center;
+`
 
 export default(props) => {
 
   return (
-    <div>Hello React</div>
+    <MiddleDiv>
+      <h3>Greeting from React + bundle in webpack</h3>
+    </MiddleDiv>
   )
+
+
 
 }
